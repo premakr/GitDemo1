3005025bf3c11bcb8e5e55852153d8a1d1e0a316
premakr <premakr86@gmail.com>
2019-07-23 21:11:21 -0700

    Update Demo1.java

--- src/Demopac/Demo1.java
+++ src/Demopac/Demo1.java
@@ -3,8 +3,10 @@ package Demopac;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		System.out.println("From Demo1");
+		System.out.println("From Demo1 in main");
+		
 
+			
 	}
 
 }
